d220ffe688df32ee3d0ba12ef26f0606a1a35e1d
Murali Krishna <murali.krishna.dms@crowdbotics.com>
2023-07-25 23:17:51 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "test-app-dev-104227",
-  url: "https://test-app-dev-104227.botics.co",
-  api: "https://test-app-dev-104227.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "test-app-dev-104227",
+  "url": "https://test-app-dev-104227.botics.co",
+  "api": "https://test-app-dev-104227.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
